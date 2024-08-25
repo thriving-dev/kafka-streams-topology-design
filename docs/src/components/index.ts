6fdaa42a3995a6f6dcd4a8f46084591bba185166
Hartmut <info@hartmut.co.uk>
2024-08-25 15:55:30 +0200

    feat: adds the kstd.thriving.dev website to public repo

---
+++ docs/src/components/index.ts
@@ -0,0 +1,8 @@
+export { default as Examples } from './Examples.astro';
+export { default as Feature } from './Feature.astro';
+export { default as Hero } from './Hero.astro';
+export { default as Highlight } from './Highlight.astro';
+export { default as Highlights } from './Highlights.astro';
+export { default as Panel } from './Panel.astro';
+export { default as ThreeCols } from './ThreeCols.astro';
+export { default as TwoCols } from './TwoCols.astro';
