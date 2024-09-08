21a55c47121d86f3220da5c03049c3f2d89f1625
Hartmut <info@hartmut.co.uk>
2024-09-08 02:00:17 +0200

    chore: remove imports for deleted 'Examples' component

--- docs/src/components/index.ts
+++ docs/src/components/index.ts
@@ -1,4 +1,3 @@
-export { default as Examples } from './Examples.astro';
 export { default as Feature } from './Feature.astro';
 export { default as Hero } from './Hero.astro';
 export { default as Highlight } from './Highlight.astro';
