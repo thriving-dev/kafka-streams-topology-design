3ad0b5c25022f8394a170fcfed1313961658ec90
Hartmut <info@hartmut.co.uk>
2025-01-05 19:37:51 +0200

    chore: prep for 1.0.0-beta.2 release

--- docs/src/globals.ts
+++ docs/src/globals.ts
@@ -1,11 +1,11 @@
 export const globals = {
-  latestVersion: '1.0.0-beta.1',
+  latestVersion: '1.0.0-beta.2',
   parent: 'https://thriving.dev',
   github: 'https://github.com/thriving-dev/kafka-streams-topology-design',
   linkedin: 'https://www.linkedin.com/in/hartmut-co-uk/',
   lib: {
-    filename: 'kafka-streams-topology-design_v1.0.0-beta.1.excalidrawlib',
-    download: 'https://raw.githubusercontent.com/thriving-dev/kafka-streams-topology-design/main/lib/kafka-streams-topology-design_v1.0.0-beta.1.excalidrawlib',
-    install: 'https://excalidraw.com/#addLibrary=https%3A%2F%2Fraw.githubusercontent.com%2Fthriving-dev%2Fkafka-streams-topology-design%2Fmain%2Flib%2Fkafka-streams-topology-design_v1.0.0-beta.1.excalidrawlib',
+    filename: 'kafka-streams-topology-design_v1.0.0-beta.2.excalidrawlib',
+    download: 'https://raw.githubusercontent.com/thriving-dev/kafka-streams-topology-design/main/lib/kafka-streams-topology-design_v1.0.0-beta.2.excalidrawlib',
+    install: 'https://excalidraw.com/#addLibrary=https%3A%2F%2Fraw.githubusercontent.com%2Fthriving-dev%2Fkafka-streams-topology-design%2Fmain%2Flib%2Fkafka-streams-topology-design_v1.0.0-beta.2.excalidrawlib',
   },
 };
