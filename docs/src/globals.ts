2b5522af9fc155ae53fd886ccf6de3a67774f4ec
Hartmut <info@hartmut.co.uk>
2025-01-31 01:19:38 +0100

    feat: adds survey banner and modal

--- docs/src/globals.ts
+++ docs/src/globals.ts
@@ -8,4 +8,10 @@ export const globals = {
     download: 'https://raw.githubusercontent.com/thriving-dev/kafka-streams-topology-design/main/lib/kafka-streams-topology-design_v1.0.0-beta.2.excalidrawlib',
     install: 'https://excalidraw.com/#addLibrary=https%3A%2F%2Fraw.githubusercontent.com%2Fthriving-dev%2Fkafka-streams-topology-design%2Fmain%2Flib%2Fkafka-streams-topology-design_v1.0.0-beta.2.excalidrawlib',
   },
+  // survey: undefined,
+  survey: {
+    title: 'Got 3 Minutes? Help Us Innovate! 💡',
+    descriptionHtml: 'We’re shaping the future of KSTD, and <strong>your insights are priceless!</strong><br/>Take a <strong>quick survey</strong> to share your thoughts and ideas.',
+    link: 'https://forms.gle/UpZcv5qvPtWt8pGn6',
+  },
 };
